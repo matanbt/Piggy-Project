6ed7388e584bf086f3fb5e0872e8d0eeb505ce2a
matanbt <63508603+matanbt@users.noreply.github.com>
2020-09-20 15:37:34 +0300

    ...

--- run.py
+++ run.py
@@ -2,6 +2,5 @@ import os
 from piggy import app
 
 if __name__ == "__main__":
-    app.config['SECRET_KEY'] = os.environ.get('SECRET')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.run(debug=True)
